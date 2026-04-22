2cb5b7a4bebf89e1463d4f3e91c57ef689b52bcb
ahaduzzamanapon <ahaduzzamanapon@gmail.com>
2026-04-22 10:39:03 +0600

    d

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -57,6 +57,7 @@
      <script async custom-element="amp-auto-ads"
         src="https://cdn.ampproject.org/v0/amp-auto-ads-0.1.js">
 </script>
+<meta name="google-adsense-account" content="ca-pub-8135122441154292">
 </head>
 <body>
 <amp-auto-ads type="adsense"
